148cc9f74c6b5f5289be075c66e8f3561b36d466
codeMatt91 <matteo.imbimbo@yahoo.it>
2022-01-26 16:56:41 +0100

    Added object in script

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
 console.log('Vue OK!', Vue);
+
+/*
+
+Descrizione:
+Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+
+Bonus:
+Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+
+*/
+
+
+Vue.config.devtools = true;
+
+const root = new Vue(
+   {
+      el: '#root',
+      data: {
+         message: 'Sono Massimo Decimo Meridio, comandante delle legioni Phenix',
+      }
+   }
+);
