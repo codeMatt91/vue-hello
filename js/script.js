2c4be3a641a25351ba197841bb440fc17dc70cb9
codeMatt91 <matteo.imbimbo@yahoo.it>
2022-01-26 18:02:17 +0100

    Added class + photo whit vuejs

--- js/script.js
+++ js/script.js
@@ -18,6 +18,11 @@ const root = new Vue(
       el: '#root',
       data: {
          message: 'Sono Massimo Decimo Meridio, comandante delle legioni Phenix',
-      }
+         font: 'roman-style',
+         immagine: "img/Il-gladiatore.webp",
+         foto: 'background'
+      },
+      
+
    }
 );
